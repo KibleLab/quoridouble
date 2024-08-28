0113261e617085b2647c72d9fad3287101d630b4
vulpes94 <rwr9857@gmail.com>
2024-08-28 14:26:29 +0900

    refactor(import): svg -> flutter_svg

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:quoridouble/screens/game_screen.dart';
 
 class HomeScreen extends StatefulWidget {
